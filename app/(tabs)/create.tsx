8a39d684a84f14ffc8aba5a6236cdccba833771e
jimcousins <jim.r.cousins@gmail.com>
2026-04-15 16:32:45 +0100

    Adding the Add Image component and the start of the text input fields

---
+++ app/(tabs)/create.tsx
@@ -0,0 +1,44 @@
+import ImageViewer from "@/components/ImageUpload";
+import InputBox from "@/components/InputBox";
+import * as ImagePicker from "expo-image-picker";
+import { useState } from "react";
+import { StyleSheet, View } from "react-native";
+
+export default function Index() {
+  const [selectImage, setSelectedImage] = useState<string | undefined>(
+    undefined,
+  );
+  const placeholderImg = require("@/assets/images/add-image-default.jpg");
+
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ["images"],
+      allowsEditing: true,
+    });
+
+    if (!result.canceled) {
+      setSelectedImage(result.assets[0].uri);
+    } else {
+      alert("You did not select any image.");
+    }
+  };
+
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <ImageViewer
+        imgSource={placeholderImg}
+        selectedImage={selectImage}
+        onPress={pickImageAsync}
+      />
+      <InputBox theme={"title"} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
