6309bc70baca50c6ad2c038791394fb9c1b8cb46
Academy Student <academy.student@lafosse.com>
2026-04-16 15:37:58 +0100

    Added Login/Signup page, and changed colour for Profile page and Nav Bar

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,21 +1,24 @@
 import { Tabs } from 'expo-router';
 
 import Ionicons from '@expo/vector-icons/Ionicons';
-import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+
 
 
 
 export default function TabLayout() {
   return (
     <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: '#ffd33d',
-      }}
-    >
+  screenOptions={{
+    tabBarActiveTintColor: '#3D5A80',
+    tabBarInactiveTintColor: '#3a3f46',
+  }}
+>
       <Tabs.Screen
         name="index"
         options={{
           title: 'Home',
+          headerShown: false,
           tabBarIcon: ({ color, focused }) => (
             <Ionicons name={focused ? 'home-sharp' : 'home-sharp'} color={color} size={24} />
           ),
@@ -25,8 +28,9 @@ export default function TabLayout() {
         name="create"
         options={{
           title: 'Create',
+          headerShown: false,
           tabBarIcon: ({ color, focused }) => (
-            <MaterialCommunityIcons name={focused ? 'close-network' : 'close-network'} size={24} color={color} />
+            <MaterialIcons name={focused ? 'add-location-alt' : 'add-location-alt'} size={24} color={color} />
           ),
         }}
       />
@@ -34,6 +38,7 @@ export default function TabLayout() {
         name="profile"
         options={{
           title: 'Profile',
+          headerShown: false,
           tabBarIcon: ({ color, focused }) => (
             <Ionicons name={focused ? 'person-sharp' : 'person-sharp'} color={color} size={24}/>
           ),
