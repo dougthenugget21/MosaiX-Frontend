3b7a66fb253b7e50b782320951db4bd608fdfdbd
olilliterate <oliver@bjerg.co.uk>
2026-04-23 15:52:48 +0100

    Fixed a navigation bug

--- components/NavButton.tsx
+++ components/NavButton.tsx
@@ -9,7 +9,7 @@ type Props = {
 export default function NavButton({ post }: Props) {
   const handlePress = async () => {
     // Checking if the link is supported for links with custom URL scheme.
-    const url = `geo:${post.latitude},${post.longitude}?q=${post.latitude},${post.latitude}(${post.title})`;
+    const url = `geo:${post.latitude},${post.longitude}?q=${post.latitude},${post.longitude}(${post.title})`;
     const supported = await Linking.canOpenURL(url);
     console.log(post);
     if (supported) {
